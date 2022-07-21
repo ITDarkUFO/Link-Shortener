fbb1cbd33587a57e206f95a7e41c9f9300bd5701
ITDarkUFO <43449316+ITDarkUFO@users.noreply.github.com>
2022-07-21 19:33:17 +0500

    Добавил модель ссылок в бд и панель админа

--- service/link_shortener/models.py
+++ service/link_shortener/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Link(models.Model):
+    original_link = models.URLField(max_length=300, verbose_name="Ссылка")
+    short_link = models.URLField(max_length=20, unique=True, verbose_name="Короткая ссылка")
+    creation_date = models.DateTimeField(verbose_name="Дата создания")
+    link_creator = models.ForeignKey(User, related_name="link_creator", on_delete=models.CASCADE, verbose_name="Пользователь")
+    following_count = models.IntegerField(default=0, verbose_name="Количество переходов")
+
+    class Meta:
+        verbose_name = 'Ссылка'
+        verbose_name_plural = 'Ссылки'
