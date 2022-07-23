d071b671d597ad9326c0ce07ccfd16c193f5d33c
ITDarkUFO <43449316+ITDarkUFO@users.noreply.github.com>
2022-07-23 16:36:38 +0500

    Добавил поля в модель бд

--- service/link_shortener/models.py
+++ service/link_shortener/models.py
@@ -3,12 +3,17 @@ from django.contrib.auth.models import User
 
 
 class Link(models.Model):
-    original_link = models.URLField(max_length=300, verbose_name="Ссылка")
+    original_link = models.URLField(max_length=300, verbose_name="Оригинальная ссылка")
     short_link = models.URLField(max_length=20, unique=True, verbose_name="Короткая ссылка")
+    
+    link_creator = models.ForeignKey(User, related_name="link_creator", null=True, on_delete=models.CASCADE, verbose_name="Пользователь")
+    session_id = models.CharField(max_length=32, verbose_name="ID сессии")
+
     creation_date = models.DateTimeField(verbose_name="Дата создания")
-    link_creator = models.ForeignKey(User, related_name="link_creator", on_delete=models.CASCADE, verbose_name="Пользователь")
-    following_count = models.IntegerField(default=0, verbose_name="Количество переходов")
+    following_count = models.PositiveIntegerField(default=0, verbose_name="Количество переходов")
+    following_date = models.DateTimeField(null=True, verbose_name="Дата последнего перехода")
+    
 
     class Meta:
-        verbose_name = 'Ссылка'
-        verbose_name_plural = 'Ссылки'
+        verbose_name = 'ссылка'
+        verbose_name_plural = 'ссылки'
