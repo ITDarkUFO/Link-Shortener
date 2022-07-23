ce0814b10b5d419f6f7da7e2e49821dfdd4662ef
ITDarkUFO <43449316+ITDarkUFO@users.noreply.github.com>
2022-07-23 16:38:36 +0500

    Добавил таблицу сокращенных ссылок

--- service/link_shortener/templatetags/templates.py
+++ service/link_shortener/templatetags/templates.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
-from django.conf import settings
-from django import template
 import logging
 
-register = template.Library()
+from django import template
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.views.decorators.csrf import csrf_protect
 
+register = template.Library()
 
 # Пример шаблона
 # @register.tag(name="temp") - если нужно
@@ -22,38 +23,6 @@ def meta(context):
     <title>Hyper - сокращатель ссылок</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link rel="shortcut icon" type="image/x-icon" href="{static}main/images/favicon.ico">
-
     <link rel="stylesheet" type="text/css" href="{static}main/styles/base.css">
     <link rel="stylesheet" type="text/css" href="{static}{page}/styles/{page}.css">
     '''
-
-@register.tag(name="signed")
-@register.simple_tag(takes_context = True)
-def header(context, signed):
-    header_content = f'''
-    <header>
-        <div class="header-logo-wrapper">
-            <div class="header-logo"></div>
-            <div class="header-text-wrapper">
-                <span>Hyper</span>
-                <span class="header-text">Сервис сокращения ссылок</span>
-            </div>
-        </div>'''
-    
-    if (signed == "True"):
-        header_content += '''
-        <div class="account-wrapper">
-            <div></div>
-            <button></button>
-        </div>
-        '''
-         
-    elif (signed == "False"):
-        header_content += '''
-        <div class="header-sign-wrapper">
-            <button>Войти</button>
-            <button>Зарегистрироваться</button>
-        </div>'''
-
-    header_content += f'''</header>'''
-    return header_content
